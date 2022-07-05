3872a90e9c8f4ec8ee0329ce0ed49c7872f7e756
currenjin <hyun0524e@naver.com>
2022-07-05 19:29:19 +0900

    init: project

---
+++ src/test/java/com/currenjin/learningtest/LearningTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currenjin.learningtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
