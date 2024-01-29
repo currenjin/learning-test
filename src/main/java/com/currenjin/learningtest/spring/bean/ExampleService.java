17d1064cd82d8ac5c8d416c80146e1f7cb3a8ec8
currenjin <hyun0524e@naver.com>
2024-01-29 12:31:00 +0900

    Add: Dependency Injection Test

---
+++ src/main/java/com/currenjin/learningtest/spring/bean/ExampleService.java
@@ -0,0 +1,10 @@
+package com.currenjin.learningtest.spring.bean;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExampleService {
+    public String getMessage() {
+        return "hi";
+    }
+}
---
+++ src/test/java/com/currenjin/learningtest/spring/bean/DependencyInjectionTest.java
@@ -0,0 +1,25 @@
+package com.currenjin.learningtest.spring.bean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class DependencyInjectionTest {
+
+    @Autowired
+    private ExampleService exampleService;
+
+    @Test
+    void dependency_injection() {
+        assertNotNull(exampleService);
+    }
+
+    @Test
+    void call_method() {
+        assertEquals("hi", exampleService.getMessage());
+    }
+}
